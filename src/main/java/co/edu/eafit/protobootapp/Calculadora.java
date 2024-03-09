190f7a3074b81d96044f6fb211e699affc493213
spatinob1-eafit <162764547+spatinob1-eafit@users.noreply.github.com>
2024-03-09 09:30:55 -0500

    rename number

--- src/main/java/co/edu/eafit/protobootapp/Calculadora.java
+++ src/main/java/co/edu/eafit/protobootapp/Calculadora.java
@@ -3,8 +3,8 @@ package co.edu.eafit.protobootapp;
 //This is a simple calculator class.
 public class Calculadora {
 
-    public Double cuadrado(Double numero) {
-        return numero * numero;
+    public Double cuadrado(Double number) {
+        return number * number;
     }
     
     public Double cubo(Double numero) {
